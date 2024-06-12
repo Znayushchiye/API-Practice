89dbbfac15cc8f5d9803cde61f10d799ec9029ac
Aaradhya joshi <aaradhyajoshi787@gmail.com>
2024-06-12 14:56:21 +0530

    Basic structure of server.js. Made config.js file.

---
+++ Backend/src/config.js
@@ -0,0 +1,6 @@
+const PORT = process.env.PORT;
+const URI = process.env.URI;
+
+const configDetails = {PORT, URI};
+
+export default configDetails;
---
+++ Backend/src/server.js
@@ -0,0 +1,40 @@
+// Imports
+import express from "express";
+import cors from "cors";
+
+import router from './routes/path.router.js';
+import configDetails from "./config.js";
+import mongoose from "mongoose";
+
+
+// Config Details
+const PORT = configDetails.PORT;
+const URI = configDetails.URI;
+
+
+// Initialisation
+const server = express();
+
+
+// Middleware
+server.use(cors);
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+
+// Routes
+server.use('/', router);
+
+
+// DB Connection
+mongoose.connect(URI)
+    .then(() => {
+        console.log('MongoDB connected successfully');
+        return server.listen(PORT);
+    })
+    .then(() => {
+        console.log(`Server listening on port ${PORT}`);
+    })
+    .catch((err) => {
+        console.error('Error connecting to DB:', err);
+    });
